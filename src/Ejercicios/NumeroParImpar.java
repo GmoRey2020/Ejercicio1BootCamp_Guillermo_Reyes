281729a0c37f7ded42db3b9140b3ee7322c194d1
Guillermo Reyes <guillermoareyess@gmail.com>
2024-09-06 12:50:33 -0400

    Sub ProductoDosNumero

---
+++ src/Ejercicios/NumeroParImpar.java
@@ -0,0 +1,14 @@
+package Ejercicios;
+public class NumeroParImpar {
+    public static void main(String[] args) {
+        int a = 15;
+        // Determinar si el número es par o impar
+        if (a % 2 == 0) {
+            // Si el residuo de a dividido por 2 es 0, entonces es par
+            System.out.println(a + " es un número par.");
+        } else {
+                // Si el residuo de a dividido por 2 no es 0, entonces es impar
+                System.out.println(a + " es un número impar.");
+        }
+    }
+}
---
+++ src/Ejercicios/ProductoDosNumero.java
@@ -0,0 +1,13 @@
+package Ejercicios;
+
+public class ProductoDosNumero {
+        public static void main(String[] args) {
+            // Declaración e inicialización de las variables
+            int a = 7;
+            int b = 8;
+            // Cálculo del producto
+            int producto = a * b;
+            // Mostrar el resultado
+            System.out.println("El producto de " + a + " y " + b + " es: " + producto);
+        }
+}
